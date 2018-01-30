3382f5dd59536eea1ac27d3b4a476c1a52fa5249
yi.li <yi.li@yulore.com>
2018-01-30 15:44:13 +0800

    优化子弹相关代码

--- src/main/kotlin/perseverance/li/game/model/Bullet.kt
+++ src/main/kotlin/perseverance/li/game/model/Bullet.kt
@@ -6,24 +6,23 @@ import perseverance.li.game.enums.Direction
 /**
  * 绘制子弹
  *
- * 这里构造里传入了函数，kotlin高阶语法
+ * 这里构造里传入了函数，kotlin特性
  */
 class Bullet(direction: Direction, calculateCoordinate: (bulletImgW: Int, bulletImgH: Int) -> Pair<Int, Int>) : View() {
 
     override var x: Int = 0
     override var y: Int = 0
-    private var bulletImg = ""
+    private val bulletImg = when (direction) {
+        Direction.UP -> "img/bullet_u.gif"
+        Direction.DOWN -> "img/bullet_d.gif"
+        Direction.LEFT -> "img/bullet_l.gif"
+        Direction.RIGHT -> "img/bullet_r.gif"
+    }
 
     init {
-        bulletImg = when (direction) {
-            Direction.UP -> "img/bullet_u.gif"
-            Direction.DOWN -> "img/bullet_d.gif"
-            Direction.LEFT -> "img/bullet_l.gif"
-            Direction.RIGHT -> "img/bullet_r.gif"
-        }
-        val bulletImgW = Painter.size(bulletImg)[0]
-        val bulletImgH = Painter.size(bulletImg)[1]
-
+        val size = Painter.size(bulletImg)
+        val bulletImgW = size[0]
+        val bulletImgH = size[1]
         val corrdinate = calculateCoordinate.invoke(bulletImgW, bulletImgH)
         x = corrdinate.first
         y = corrdinate.second
